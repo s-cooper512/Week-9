5ed909794897d229d840c6fd98bd67c9050c54c9
Stephen Cooper <scooper@indeed.com>
2024-04-16 10:04:45 -0500

    Week 10: Day 1 lab (on Day 2): I've been trapped down here in the code mines for so long that the days have begun to run together. I can only hope that the rescue team finds me soon.

--- Day_1/Homework/SchoolApp/src/main/java/org/example/schoolapp/Student/Address.java
+++ Day_1/Homework/SchoolApp/src/main/java/org/example/schoolapp/Student/Address.java
@@ -16,4 +16,56 @@ public class Address {
         private String city;
         private String country;
         private String postalCode;
+
+        public Address(Long id, String street, String city, String country, String postalCode) {
+                this.id = id;
+                this.street = street;
+                this.city = city;
+                this.country = country;
+                this.postalCode = postalCode;
+        }
+
+        public Address() {
+
+        }
+
+        public Long getId() {
+                return id;
+        }
+
+        public void setId(Long id) {
+                this.id = id;
+        }
+
+        public String getStreet() {
+                return street;
+        }
+
+        public void setStreet(String street) {
+                this.street = street;
+        }
+
+        public String getCity() {
+                return city;
+        }
+
+        public void setCity(String city) {
+                this.city = city;
+        }
+
+        public String getCountry() {
+                return country;
+        }
+
+        public void setCountry(String country) {
+                this.country = country;
+        }
+
+        public String getPostalCode() {
+                return postalCode;
+        }
+
+        public void setPostalCode(String postalCode) {
+                this.postalCode = postalCode;
+        }
 }
